a7358f6ffa4a1354234110ce63eb0168e5734c5a
paperjuice <paperjuiceinc@gmail.com>
2017-09-02 23:01:48 +0100

    Add texture class

---
+++ OpenGL1/Texture.h
@@ -0,0 +1,20 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <GL/glew.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+
+class Texture{
+public:
+
+
+};
+
+
+
+
+#endif
